b739fd83151965bdf7ee93c99b48b0e33b37ce31
朝阳 <603092378@qq.com>
2021-03-10 20:46:06 +0800

    增加代码字体

--- src/components/s-code.vue
+++ src/components/s-code.vue
@@ -1,34 +1,34 @@
 需安装插件——代码高亮  "vue-highlightjs": "^1.3.3"    "highlight.js": "^9.18.0", 和 复制     "vue-clipboard2": "^0.3.1",
 <template>
-    <pre class="codeBox" v-highlightjs><code :class="lan">{{code}}</code>
+  <pre class="codeBox" v-highlightjs><code :class="lan">{{code}}</code>
     <button class="copyBtn" round size="mini" v-clipboard:copy="code" v-clipboard:success="onCopy">复制</button></pre>
 </template>
 <script>
-    export default {
-        props: {
-            code: String,
-            lan: String
-        },
-        methods: {
-            onCopy() {
-     
-
-              this.$info('复制成功！')
-      
-            }
-        }
-    }
+export default {
+  props: {
+    code: String,
+    lan: String,
+  },
+  methods: {
+    onCopy() {
+      this.$info("复制成功！");
+    },
+  },
+};
 </script>
 <style scoped>
-.codeBox{
-    position: relative;
-    margin-right: 10px;
-    min-height: 45px;
-    background: #23241f;
+.codeBox {
+  position: relative;
+  margin-right: 10px;
+  min-height: 45px;
+  background: #23241f;
 }
-.copyBtn{
-    position: absolute;
-    right: 8px;
-    top:8px;
+.copyBtn {
+  position: absolute;
+  right: 8px;
+  top: 8px;
+}
+code{
+    font-family: 'Camingo Code';
 }
 </style>
